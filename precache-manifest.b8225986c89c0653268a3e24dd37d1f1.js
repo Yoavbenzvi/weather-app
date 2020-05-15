45a23b3c4a53b650fdb9da4b83acbc5156094e47
Yoav <yoav.benzvi@gmail.com>
2020-05-15 21:02:47 +0200

    Updates

---
+++ precache-manifest.b8225986c89c0653268a3e24dd37d1f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b65e997392bcb401f9440aab6ce71d",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "032fbacf7a5e419b6899",
+    "url": "/weather-app/static/css/main.1b58b84e.chunk.css"
+  },
+  {
+    "revision": "ef7e779cd07d6587f4df",
+    "url": "/weather-app/static/js/2.6acc00e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.6acc00e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "032fbacf7a5e419b6899",
+    "url": "/weather-app/static/js/main.6fc49aea.chunk.js"
+  },
+  {
+    "revision": "a07bf81efcef36e3f256",
+    "url": "/weather-app/static/js/runtime-main.443c5861.js"
+  }
+]);
